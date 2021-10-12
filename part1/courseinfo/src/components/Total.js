9f53f65c50e2f04173cab86fcdf389f6a80dac62
mavhungutrezzy <mavhungutrezzy@gmail.com>
2021-10-12 13:25:43 +0200

    first commit

---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Total = (props) => {
+    
+    return (
+        <div>
+            <p> Number of exercises { exercise1 +  exercise2 + exercise3 } </p>
+        </div>
+    )
+    
+}
+
+
+export default Total;
